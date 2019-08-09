72ab484dd6a4c0f5b07756d703542642f173ed0a
Cloude <boxbox557@gmail.com>
2019-08-09 16:52:15 +0800

    Beautify index appearence.

--- server/static/script/script.js
+++ server/static/script/script.js
@@ -1,7 +1,25 @@
+/* element */
+var alertBlock = null;
+var statusBlock = null;
+var input_url = null;
+var btn_submit = null;
 
+/* properties */
+var BTN_CLICKED = false;
+var ALERT_TIME = 2000;	// 2s
+
+/**
+ * comment
+ */
 function ErrorHandler(e) {
 	console.log("Error: " + e);
-	$("#status").html("<p><span class=\"badge badge-danger\">Error</span>Download fail.</p>");
+	
+	BTN_CLICKED = false;
+	btn_submit.removeClass("disabled");
+	$("#status").html("<p><span class=\"badge badge-danger\">Error</span><span id=\"status-content\">Download fail.</span></p>");
+	alertBlock.html("<div class=\"alert alert-danger\">Something wrong. Please try again.</div>")
+	alertBlock.fadeIn("slow");
+	setTimeout(()=>{alertBlock.fadeOut("slow")}, ALERT_TIME);
 }
 
 function GenerateDownloadLink(data) {
@@ -10,46 +28,56 @@ function GenerateDownloadLink(data) {
 	// content = "data:text/plain;charset=UTF-8," + encodeURIComponent(data[result]);
 	content = "data:text/plain;charset=UTF-8," + encodeURIComponent("Success!");
 	
-	// show download button
 	$("#status").html("<a type=\"button\" download=\"content.txt\" class=\"btn btn-secondary\" id=\"download-btn\">Download</a>");
 	$("#download-btn").attr("href", content);
+	BTN_CLICKED = false;
+	btn_submit.removeClass("disabled");
+	alertBlock.html("<div class=\"alert alert-success\">Request success! You can download the file now.</div>")
+	alertBlock.fadeIn("slow");
+	setTimeout(()=>{alertBlock.fadeOut("slow")}, ALERT_TIME);
 }
 
 function RequestData(rqType, rqUrl, rqData) {
 	console.log("Requesting data...");
 	// show loading icon
-	$("#status").html("<div class=\"spinner-border text-success\"><span class=\"sr-only\">Loading...</span></div>");
+	$("#status").html("<div class=\"spinner-border text-success\"/><span class=\"text-dark\" id=\"status-content\">Loading...</span>");
 	
-	// get data 
-	// $.getJSON(
-		// rqUrl,
-		// {link: rqData},
-		// GenerateDownloadLink
-	// );
-	$.ajax({
-		url: rqUrl,
-		type: rqType,
-		dataType: 'json',
-		contentType: 'application/json',
-		data: JSON.stringify({"link":rqData}),
-		success: GenerateDownloadLink,
-		error: ErrorHandler
-	});
+	/* get data */
+	// $.ajax({
+		// url: rqUrl,
+		// type: rqType,
+		// dataType: 'json',
+		// contentType: 'application/json',
+		// data: JSON.stringify({"link":rqData}),
+		// success: GenerateDownloadLink,
+		// error: ErrorHandler
+	// });
 	
 	// GenerateDownloadLink(rqData);
 	// ErrorHandler("test");
 }
 
 function SubmitClick(event) {
-	url = $("#plurk-url").val();
-	// TODO:
-	//   do input examining to prevent attack
-	RequestData("POST", "http://140.114.195.85:8765/backup", url)
+	if(!BTN_CLICKED){
+		BTN_CLICKED = true;
+		btn_submit.addClass("disabled");
+		
+		url = $("#plurk-url").val();
+		// TODO:
+		//   do input examining to prevent attack
+		RequestData("POST", "http://140.114.195.85:8765/backup", url)
+	}
 }
 
 function onload() {
-	console.log("page ready.")
-	$("#plurk-url-submit").click(SubmitClick);
+	/* get component */
+	alertBlock = $("#alert");
+	statusBlock = $("#status");
+	btn_submit = $("#plurk-url-submit");
+	
+	/* page ready, registor event respectively */
+	btn_submit.click(SubmitClick);
+	alertBlock.removeClass("d-none").hide();
 }
 
 $(document).ready(onload);

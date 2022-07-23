66816766fd68ff7db7ebe6812e713c8b1d972dc0
cloude <boxbox557@gmail.com>
2022-07-23 17:45:49 +0800

    feat: handle plurk url as input

--- api/application/controller/app.py
+++ api/application/controller/app.py
@@ -1,3 +1,6 @@
+import logging
+import re
+
 from fastapi import FastAPI, HTTPException
 
 from ..form import ExtractRequest
@@ -8,6 +11,7 @@ from ...infrastructure.plurk.plurk_api import get_plurk, get_response
 
 
 app = FastAPI()
+logger = logging.getLogger('api')
 
 
 @app.get('/', status_code=200)
@@ -16,21 +20,27 @@ def index():
 
 
 @app.get('/markdown', response_model=PlurkMD)
-def get_markdown(request: str):
-    # WIP: plurk content is returned directly for now
-    # TODO: return processed markdown instead
-    if request.startswith('http'):
-        ... # TODO: handle url
-    elif request.isdigit():
-        rq = ExtractRequest(plurk_id=int(request))
-    elif is_b36_str(request):
-        rq = ExtractRequest(plurk_id_base36=request)
+def get_markdown(q: str):
+    logger.info(f'got request: {q}')
+
+    if re.match(r'^((http|https):\/\/)?www.plurk.com(\/m)?\/p\/([\da-z]+)\/?$', q):
+        logger.info(f'treats request string as a plurk url: {q}')
+        _, plurk_id = q.rstrip('/').rsplit('/', 1)
+        rq = ExtractRequest(plurk_id_base36=plurk_id)
+    elif q.isdigit():
+        logger.info(f'treats request string as a plurk id: {int(q)}')
+        rq = ExtractRequest(plurk_id=int(q))
+    elif is_b36_str(q):
+        logger.info(f'treats request string as an encoded plurk id: {q}')
+        rq = ExtractRequest(plurk_id_base36=q)
     else:
         raise HTTPException(status_code=400, detail='invalid input')
 
+    logger.info(f'got encoded plurk id: {rq.plurk_id}')
+
+    # TODO: handle errors
     p = get_plurk(rq)
     p = get_response(p)
-
     md = gen_markdown_from_plurk(p)
 
     return md

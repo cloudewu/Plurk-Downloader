cf84b6d4c9d452db05e77c6cf9b462bc165991c0
cloude <boxbox557@gmail.com>
2022-07-25 01:18:30 +0800

    feat: add welcome text

--- app/src/pages/index.tsx
+++ app/src/pages/index.tsx
@@ -2,6 +2,7 @@ import Head from 'next/head';
 import * as React from 'react';
 
 import InputForm from '../components/home/InputForm';
+import WelcomePanel from '../components/home/WelcomePanel';
 
 function HomePage() {
   return (
@@ -11,6 +12,8 @@ function HomePage() {
       </Head>
 
       <div className="mx-auto max-w-screen-md border border-red-500 px-6 py-20">
+        <h1 className="text-lg font-bold">噗文轉存器</h1>
+        <WelcomePanel className="my-8" />
         <InputForm />
       </div>
     </>
